d570a3ddfaed126f675969c1c47bcef813a166f8
Maxim Moskalyk <ictic.step@gmail.com>
2025-04-04 22:34:00 +0300

    vite confi updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/University_laboratory_WebCICD_1/',
   plugins: [react()],
-})
+});
